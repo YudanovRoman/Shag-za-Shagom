16930bea21e65b6b7f87ab2b1908a51ab096ca98
Farewwel <romanjowow@gmail.com>
2025-05-09 22:44:27 +0300

    создал настоящую защиту паролей, сайт подправил, аккаунты работают(но есть небольшая ошибка с формой)

--- server.py
+++ server.py
@@ -2,7 +2,10 @@ from flask import Flask
 from flask import render_template, request, make_response
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo, ValidationError
+import sqlite3
+from pprint import pprint
+from security import generate_hash, check_password_by_username
 from http import cookies
 from datetime import time, datetime
 
@@ -10,12 +13,95 @@ from datetime import time, datetime
 class LoginForm(FlaskForm):
     email = StringField('Адрес ел. Почты', validators=[DataRequired()])
     username = StringField('Имя пользователя', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    remember_me = BooleanField('Запомнить меня')
-    submit = SubmitField('Войти')
+    password = PasswordField('Пароль', [
+        DataRequired(),
+        EqualTo('confirm', message='Пароли должны совпадать')
+    ])
+    confirm = PasswordField('Повторите пароль')
+    submit = SubmitField('Зарегистрироваться')
+
+    def validate_username(form, field):
+        print(52)
+        con = sqlite3.connect(f'static/sqLite3/{TABLE}.db')
+        cur = con.cursor()
+        usernames = cur.execute('''SELECT username FROM accounts''').fetchall()
+        print(field.data)
+        if field.data in usernames:
+            print('fuck ebat')
+            raise ValidationError('this username is already exists')
+        con.close()
+
+    def validate_email(form, field):
+        con = sqlite3.connect(f'static/sqLite3/{TABLE}.db')
+        cur = con.cursor()
+        emails = cur.execute('''SELECT email FROM accounts''').fetchall()
+        if field.data in emails:
+            raise ValidationError('this email is already exists')
+        con.close()
+
+
+class AutoForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    password = PasswordField('Пароль', [DataRequired()])
+    submit = SubmitField('Зарегистрироваться')
+
+    def validate_username(form, field):
+        con = sqlite3.connect(f'static/sqLite3/{TABLE}.db')
+        cur = con.cursor()
+        usernames = cur.execute('''SELECT username FROM accounts''').fetchall()
+        con.close()
+        if field.data not in usernames:
+            raise ValidationError('this username is not exists')
+
+    def validate_password(form, field):
+        username = form.username.data
+        if not check_password_by_username(field.data, username):
+            raise ValidationError('username or password is wrong')
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+TABLE = 'data'
+DYNAMIC_SOLT_TABLE = 'security_data'
+
+
+def theme_master(response):
+    if 'theme' not in request.cookies:
+        response.set_cookie('theme', 'light', max_age=60*60*24*2)
+
+
+def new_account(con, email, username, password):
+    cur = con.cursor()
+    max_id = cur.execute('''SELECT MAX(id) FROM accounts''').fetchone()[0]
+    if max_id is None:
+        new_id = 1
+    else:
+        new_id = max_id + 1
+    hash_data = generate_hash(password, new_id)
+    cur.execute(f'INSERT INTO accounts(email,username,password_hash)'
+                f' VALUES(\'{email}\',\'{username}\',\'{hash_data}\')')
+    con.commit()
+    return str(new_id)
+
+
+def answer_format(ans: str):
+    res = ans[ans.rfind('=') + 1:ans.rfind('"') + 1]
+    return res
+
+
+def get_account_info(con, account_id):
+    print(account_id)
+    cur = con.cursor()
+    username, email = cur.execute('''SELECT username,email FROM accounts WHERE id=?''', account_id).fetchone()
+    routes_id = cur.execute('''SELECT id FROM routes WHERE creator_id=?''', account_id).fetchall()
+    res = {
+        'username': username,
+        'email': email,
+        'routes_id': routes_id
+    }
+    pprint(res)
+    return res
 
 
 @app.route('/')
@@ -27,7 +113,7 @@ def title():
     else:
         temp = 'Profile'
     res = make_response(render_template('title.html', title='Welcome', autorization=temp))
-    res.set_cookie('theme', 'light', max_age=60*60*24)
+    theme_master(res)
     return res
 
 @app.route('/Info')
@@ -37,31 +123,46 @@ def get_info():
         temp = 'Register'
     else:
         temp = 'Profile'
-    return render_template('information.html', title='Information', autorization=temp)
+    res = make_response(render_template('information.html', title='Information', autorization=temp))
+    theme_master(res)
+    return res
 
 @app.route('/Account', methods=['GET', 'POST'])
 def register():
     account_id = request.cookies.get('account_id')
+    con = sqlite3.connect(f'static/sqLite3/{TABLE}.db')
+
     if not account_id or account_id == '-1':
         form = LoginForm()
         if form.validate_on_submit():
-            temp_id = 228
-            res = make_response(render_template('profile.html', title='My profile',
-                                autorization='Profile', account_id=str(temp_id)))
-            res.set_cookie('account_id', '228', max_age=60 * 60 * 24 * 365 * 2)
+            res = make_response(render_template('profile.html',
+                                                title='My profile', autorization='Profile',
+                                                email=form.email, username=form.username))
+            res.set_cookie('account_id', new_account(con, form.email.data, form.username.data, form.password.data),
+                           max_age=60 * 60 * 24 * 365 * 2)
             print(form.username)
-            # тут данные на баазу
+            con.close()
+            theme_master(res)
             return res
         print(0)
         res = make_response(render_template('register.html', title='Register',
                                             autorization='Register', form=form))
         res.set_cookie('account_id', '-1', max_age=60 * 60 * 24 * 365 * 2)
+        theme_master(res)
+        con.close()
         return res
     else:
-        pass
+        account_data = get_account_info(con, account_id)
         res = make_response(render_template('profile.html', title='My profile',
-              autorization='Profile', account_id=account_id))
+                                            autorization='Profile', username=account_data['username'],
+                                            email=account_data['email'], created_routes=account_data['routes_id']))
+        theme_master(res)
+        con.close()
         return res
+
+@app.route('/Route/<route_id>')
+def route(route_id):
+    pass
 
 
 if __name__ == '__main__':
--- static/base.js
+++ static/base.js
@@ -1,17 +1,38 @@
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+  }
+
 function toggleMenu() {
     let navbar = document.getElementById("myNavbar");
     navbar.classList.toggle("responsive");
 }
+
+const delay = 150;
+const currentTheme = getCookie('theme');
+if (currentTheme === 'dark') {
+    document.body.className = 'dark-theme';
+    document.getElementById("imgClickAndChange").src = "static/img/moon_button.png";
+    document.getElementById('theme-toggle').classList.replace("light", "dark");
+} else {
+    document.body.className = 'light-theme';
+    document.getElementById("imgClickAndChange").src = "static/img/sun_button.png";
+    document.getElementById('theme-toggle').classList.replace("dark", "light");
+}
+
 document.getElementById('theme-toggle').addEventListener('click', function() {
     const currentTheme = document.body.className;
     if (currentTheme === 'light-theme') {
+        document.cookie = "theme=dark";
         document.body.className = 'dark-theme';
         document.getElementById("imgClickAndChange").src = "static/img/moon_button.png";
-        getElementById('theme-toggle').classList.replace("light", "dark");
+        document.getElementById('theme-toggle').classList.replace("light", "dark");
     } else {
+        document.cookie = "theme=light";
         document.body.className = 'light-theme';
         document.getElementById("imgClickAndChange").src = "static/img/sun_button.png";
-        getElementById('theme-toggle').classList.replace("dark", "light");
+        document.getElementById('theme-toggle').classList.replace("dark", "light");
     }
-    console.log("Switching theme");
-}); 
+});
